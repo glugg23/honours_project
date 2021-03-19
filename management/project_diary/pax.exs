d4a600eeaa9cdf9bee1f85d23505b7c818e5b41d
Max Leonhardt <m.leonhardt424@gmail.com>
2021-03-19 21:17:53 +0000

    Do end of week tasks

--- management/project_diary/pax.exs
+++ management/project_diary/pax.exs
@@ -1,9 +1,8 @@
 # This script generates .pax files for all the pdfs in this folder
-command = fn x -> System.cmd("pdfannotextractor", [x]) end
 
 tasks =
   File.ls!()
   |> Enum.filter(fn f -> f =~ ~r".*\.pdf$" end)
-  |> Enum.map(fn f -> Task.async(fn -> command.(f) end) end)
+  |> Enum.map(&Task.async(fn -> System.cmd("pdfannotextractor", [&1]) end))
 
 Task.yield_many(tasks, :infinity)
