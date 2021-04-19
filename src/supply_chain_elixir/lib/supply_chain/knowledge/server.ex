55ceda7fd560ec41c8a5beb49dc62d150404219c
Max Leonhardt <m.leonhardt424@gmail.com>
2021-04-19 18:02:30 +0100

    Get agent type from system env

--- src/supply_chain_elixir/lib/supply_chain/knowledge/server.ex
+++ src/supply_chain_elixir/lib/supply_chain/knowledge/server.ex
@@ -1,14 +1,28 @@
 defmodule SupplyChain.Knowledge.Server do
   @moduledoc false
 
+  require Logger
+
   use GenServer
 
   def init(_args) do
-    state = [config: Application.get_env(:supply_chain, Agent)]
+    type = System.get_env("AGENT_TYPE") |> match_config_type()
+    state = [config: Application.get_env(:supply_chain, type)]
     {:ok, state}
   end
 
   def handle_call(:get_config, _from, state) do
     {:reply, state[:config], state}
+  end
+
+  defp match_config_type(string) do
+    case string do
+      "agent" ->
+        Agent
+
+      _ ->
+        Logger.critical("Invalid string #{string} for AGENT_TYPE environment variable")
+        System.stop(1)
+    end
   end
 end
---
+++ src/supply_chain_elixir/repl.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Development script to start iex with a given node name and agent type
+
+NAME=${1-agent}
+export AGENT_TYPE=${2-agent}
+
+iex --sname $NAME -S mix
