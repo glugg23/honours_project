dd81194d3114f52a5907f5de52f4a4e41df94d6a
Max Leonhardt <m.leonhardt424@gmail.com>
2021-09-15 14:12:44 +0100

    Attempt to send messages to remote agent
    The GUID name of the agent seems to have to be an IP address.
    This is bad because we can not control the IP address given by Docker.
    Will ask if there is a better way to do this, or if I need to do
    runtime DNS resolving.

--- src/supply_chain_jade/src/main/java/uk/ac/napier/Main.java
+++ src/supply_chain_jade/src/main/java/uk/ac/napier/Main.java
@@ -1,10 +1,31 @@
 package uk.ac.napier;
 
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.lang.acl.ACLMessage;
 
 public class Main extends Agent {
     @Override
     protected void setup() {
-        System.out.println("Hello from " + this.getAID().getName());
+        OneShotBehaviour behaviour = new OneShotBehaviour(this) {
+            @Override
+            public void action() {
+                System.out.println("Hello from " + myAgent.getAID().getName());
+                ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+                AID aid = new AID("clock@172.20.0.2:1099/JADE", AID.ISGUID);
+                aid.addAddresses("http://clock:7778/acc");
+                msg.addReceiver(aid);
+                msg.setContent("Hello!");
+                send(msg);
+
+                for(int i = 0; i < 4; i++) {
+                    ACLMessage reply = blockingReceive();
+                    System.out.println(reply.getContent());
+                }
+            }
+        };
+
+        this.addBehaviour(behaviour);
     }
 }
