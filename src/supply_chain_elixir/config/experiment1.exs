9e61c6105f79b8226c8f506b35c204e6238b0771
Max Leonhardt <m.leonhardt424@gmail.com>
2021-10-28 22:55:14 +0100

    Add config for second experiment

---
+++ src/supply_chain_elixir/config/experiment1.exs
@@ -0,0 +1,31 @@
+import Config
+
+config :supply_chain, :common,
+  components: [good: 1650],
+  computers: [computer: 1650],
+  recipes: [computer: [good: 1]]
+
+config :supply_chain, :clock,
+  type: :clock,
+  information_filter: [],
+  agent_count: 4,
+  max_rounds: 220,
+  automatic?: true
+
+config :supply_chain, :manufacturer,
+  type: :manufacturer,
+  information_filter: [],
+  production_capacity: 2000,
+  producer_capacity: 550
+
+config :supply_chain, :consumer,
+  type: :consumer,
+  information_filter: [:producer]
+
+config :supply_chain, :producer,
+  type: :producer,
+  information_filter: [:consumer, :producer],
+  produces: :good,
+  production_capacity: 550
+
+config :supply_chain, :agent_types, [:clock, :manufacturer, :consumer, :producer]
