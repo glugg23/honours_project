ffcbe87d884578d2426aa7cc588d45efcdf44faf
Max Leonhardt <m.leonhardt424@gmail.com>
2021-11-02 21:53:18 +0000

    In-progress: handle ordering new components

--- src/supply_chain_jade/src/main/java/uk/ac/napier/behaviour/Manufacturer.java
+++ src/supply_chain_jade/src/main/java/uk/ac/napier/behaviour/Manufacturer.java
@@ -9,10 +9,7 @@ import jade.lang.acl.UnreadableException;
 import uk.ac.napier.util.*;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -23,6 +20,7 @@ public class Manufacturer extends Agent {
     final static Logger logger = Logger.getLogger(Manufacturer.class.getName());
     private static final String START_ROUND = "startRound";
     private static final String HANDLE_NEW_ORDERS = "handleNewOrders";
+    private static final String HANDLE_NEW_COMPONENTS = "handleNewComponents";
     private static final String SEND_FINISH_MSG = "sendFinishMsg";
     private State state;
     private ACLMessage roundMsg;
@@ -33,10 +31,12 @@ public class Manufacturer extends Agent {
 
         fsm.registerFirstState(new StartRound(this), START_ROUND);
         fsm.registerState(new HandleNewOrders(this), HANDLE_NEW_ORDERS);
+        fsm.registerState(new HandleNewComponents(this), HANDLE_NEW_COMPONENTS);
         fsm.registerState(new SendFinishMsg(this), SEND_FINISH_MSG);
 
         fsm.registerDefaultTransition(START_ROUND, HANDLE_NEW_ORDERS);
-        fsm.registerDefaultTransition(HANDLE_NEW_ORDERS, SEND_FINISH_MSG);
+        fsm.registerDefaultTransition(HANDLE_NEW_ORDERS, HANDLE_NEW_COMPONENTS);
+        fsm.registerDefaultTransition(HANDLE_NEW_COMPONENTS, SEND_FINISH_MSG);
         fsm.registerDefaultTransition(SEND_FINISH_MSG, START_ROUND);
 
         this.addBehaviour(fsm);
@@ -83,6 +83,8 @@ public class Manufacturer extends Agent {
                     .sorted(Comparator.comparing(Mail::getRequest).reversed())
                     .collect(Collectors.toList());
 
+            // TODO: Race condition, inbox is a snapshot of messages and buying orders may not have arrived
+
             int usedProduction = 0;
             ArrayList<Order> buyingRequests = new ArrayList<>(messages.size());
 
@@ -118,6 +120,45 @@ public class Manufacturer extends Agent {
         private boolean acceptablePrice(State state, Request request) {
             HashMap<String, Integer> computers = state.getComputers();
             return request.getPrice() >= computers.get(request.getGood());
+        }
+    }
+
+    private static class HandleNewComponents extends OneShotBehaviour {
+        private final Manufacturer manufacturer;
+
+        public HandleNewComponents(Manufacturer a) {
+            super(a);
+            manufacturer = a;
+        }
+
+        @Override
+        public void action() {
+            List<Order> orders = manufacturer.state.getOrders().values().stream()
+                    .filter(o -> o.getRound() == manufacturer.state.getRound())
+                    .collect(Collectors.toList());
+
+            HashMap<String, Integer> requiredComponents = new HashMap<>();
+            HashMap<String, HashMap<String, Integer>> recipes = manufacturer.state.getRecipes();
+
+            for(Order o : orders) {
+                int computerQuantity = o.getRequest().getQuantity();
+
+                for(Map.Entry<String, Integer> component : recipes.get(o.getRequest().getGood()).entrySet()) {
+                    Integer alreadyRequired = requiredComponents.getOrDefault(component.getKey(), 0);
+                    Integer quantity = computerQuantity * component.getValue();
+                    requiredComponents.put(component.getKey(), alreadyRequired + quantity);
+                }
+            }
+
+            for(Map.Entry<String, Integer> component : requiredComponents.entrySet()) {
+                List<Mail> sellingOrders = manufacturer.state.getInbox().values().stream()
+                        .filter(m -> m.getRequest().getType().equals("selling"))
+                        .filter(m -> m.getRequest().getGood().equals(component.getKey()))
+                        .sorted(Comparator.comparing(Mail::getRequest).reversed())
+                        .collect(Collectors.toList());
+
+                // TODO: Race condition, inbox is a snapshot of messages and selling orders may not have arrived
+            }
         }
     }
 
