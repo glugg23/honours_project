9ca228c93a6fc290eee3b63147bf509eaf3cc330
Max Leonhardt <m.leonhardt424@gmail.com>
2021-11-04 10:17:24 +0000

    Format Elixir scripts

--- management/project_diary/pax.exs
+++ management/project_diary/pax.exs
@@ -1,10 +1,10 @@
-#!/usr/bin/env elixir
+#! /usr/bin/env elixir
 
 # This script generates .pax files for all the pdfs in this folder
 
 tasks =
   File.ls!()
-  |> Enum.filter(&Path.extname(&1) == ".pdf")
+  |> Enum.filter(&(Path.extname(&1) == ".pdf"))
   |> Enum.map(&Task.async(fn -> System.cmd("pdfannotextractor", [&1]) end))
 
 Task.yield_many(tasks, :infinity)
