f24ffc4ecd26dbad517ee7422d5b649912d4790d
Max Leonhardt <m.leonhardt424@gmail.com>
2021-11-07 18:38:22 +0000

    Add script to run experiments

---
+++ src/supply_chain_jade/experiment.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+DOCKER_FILE=${1}
+
+echo "Building Docker images and doing warmup run"
+
+docker compose -f $DOCKER_FILE up --build --remove-orphans > /dev/null 2>&1
+
+for i in $(seq 10)
+do
+    echo "Run $i"
+    docker compose -f $DOCKER_FILE up > "$i.txt" 2> /dev/null
+done
+
+echo "Moving logs to ../../dissertation/charts/"
+for i in $(seq 10)
+do
+    mv $i.txt ../../dissertation/charts/
+done
