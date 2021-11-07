e5f86eae3f0bd587645df54c22a5ccc03a72c836
Max Leonhardt <m.leonhardt424@gmail.com>
2021-11-07 21:08:55 +0000

    Add JADE results to existing charts

--- dissertation/charts/charts.R
+++ dissertation/charts/charts.R
@@ -6,14 +6,23 @@ elixir3 <- read.csv("elixir/experiment3.csv")
 elixir4 <- read.csv("elixir/experiment4.csv")
 elixir5 <- read.csv("elixir/experiment5.csv")
 
-system <- rep(c("Elixir"), 5)
+jade1 <- read.csv("jade/experiment1.csv")
+jade2 <- read.csv("jade/experiment2.csv")
+jade3 <- read.csv("jade/experiment3.csv")
+jade4 <- read.csv("jade/experiment4.csv")
+jade5 <- read.csv("jade/experiment5.csv")
+
+system <- rep(c("Elixir", "JADE"), each = 5)
 
 experiment <-
-    c("Experiment 1",
-      "Experiment 2",
-      "Experiment 3",
-      "Experiment 4",
-      "Experiment 5")
+    rep(c(
+        "Experiment 1",
+        "Experiment 2",
+        "Experiment 3",
+        "Experiment 4",
+        "Experiment 5"
+    ),
+    2)
 
 mean_cpu <-
     c(
@@ -21,13 +30,18 @@ mean_cpu <-
         mean(elixir2$cpu_usage[-(1:1)]),
         mean(elixir3$cpu_usage[-(1:1)]),
         mean(elixir4$cpu_usage[-(1:1)]),
-        mean(elixir5$cpu_usage[-(1:1)])
+        mean(elixir5$cpu_usage[-(1:1)]),
+        mean(jade1$cpu_usage[-(1:1)]),
+        mean(jade2$cpu_usage[-(1:1)]),
+        mean(jade3$cpu_usage[-(1:1)]),
+        mean(jade4$cpu_usage[-(1:1)]),
+        mean(jade5$cpu_usage[-(1:1)])
     )
 
 cpu_df <- data.frame(system, experiment, mean_cpu)
 
 ggplot(cpu_df, aes(x = experiment, y = mean_cpu, fill = system)) +
-    geom_col() +
+    geom_col(position = "dodge") +
     theme_bw() +
     labs(x = "Experiment", y = "Mean CPU utilisation (%)", fill = "System")
 
@@ -37,12 +51,17 @@ mean_memory <-
         mean(elixir2$memory[-(1:1)]) / 1024 / 1024,
         mean(elixir3$memory[-(1:1)]) / 1024 / 1024,
         mean(elixir4$memory[-(1:1)]) / 1024 / 1024,
-        mean(elixir5$memory[-(1:1)]) / 1024 / 1024
+        mean(elixir5$memory[-(1:1)]) / 1024 / 1024,
+        mean(jade1$memory[-(1:1)]) / 1024 / 1024,
+        mean(jade2$memory[-(1:1)]) / 1024 / 1024,
+        mean(jade3$memory[-(1:1)]) / 1024 / 1024,
+        mean(jade4$memory[-(1:1)]) / 1024 / 1024,
+        mean(jade5$memory[-(1:1)]) / 1024 / 1024
     )
 
 memory_df <- data.frame(system, experiment, mean_memory)
 
 ggplot(memory_df, aes(x = experiment, y = mean_memory, fill = system)) +
-    geom_col() +
+    geom_col(position = "dodge") +
     theme_bw() +
     labs(x = "Experiment", y = "Mean memory usage (MiB)", fill = "System")
